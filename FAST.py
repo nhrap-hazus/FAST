82e43251fadb9c5cb6d7217cb151487821d2f155
ujvalaks <ujvalak_in@yahoo.com>
2020-02-27 22:14:34 -0800

    Delete .bat if exist

--- FAST.py
+++ FAST.py
@@ -4,4 +4,9 @@
 # REM A single & sign executes the first command and moves forwards regardless
 # call conda.bat activate hazus_env & start /min python .\src\FAST_run.py && exit
 from subprocess import call
+import os
 call('CALL conda.bat activate hazus_env & start /min python ./src/FAST_run.py', shell=True)
+if os.path.exists("FAST.bat"):
+    os.remove("FAST.bat")
+else:
+    exit(0)
--- FAST_Preprocessing.py
+++ FAST_Preprocessing.py
@@ -2,4 +2,9 @@
 # REM conda activate hazus_env & python .\python_env\FAST_PreProcess_run.py
 # call conda.bat activate hazus_env & start /min python .\src\FAST_PreProcess_run.py && exit
 from subprocess import call
+import os
 call('CALL conda.bat activate hazus_env & start /min python ./src/FAST_PreProcess_run.py', shell=True)
+if os.path.exists("FAST_Preprocessing.bat"):
+    os.remove("FAST_Preprocessing.bat")
+else:
+    exit(0)
