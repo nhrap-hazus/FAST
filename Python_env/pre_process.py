d9f96b64fbd9006b273906bc80b8dec39cb95927
ujvalaks <ujvalak_in@yahoo.com>
2020-04-13 11:42:06 -0700

    RTC Task 35520 and DR 35550- FAST Custom DDFs
    The building DDFs were getting the values of the Content and the Content DDFs were being wiped out completely.

--- Python_env/pre_process.py
+++ Python_env/pre_process.py
@@ -1,10 +1,8 @@
 import os, csv
 
 dir = os.getcwd()
-if (dir.find('Python_env')!= -1):
-     dir = os.path.dirname(dir)
-# dir = os.path.dirname(dir)
-# print(dir)
+dir = os.path.dirname(dir)
+
 def process(input,fmap):
     try:
         output = input.split('.')[0]+'_pre_processed.csv'
@@ -16,12 +14,10 @@ def process(input,fmap):
         IDDF_ID = IDDF_ID if IDDF_ID != '' else 'IDDF_ID'
         
         LUT_Dir = os.path.join(dir,'lookuptables')
-        # print(LUT_Dir)
+        
         DDFAssign = ['SOoccupId_Occ_Xref','flBldgStructDmgFinal','flBldgContDmgFinal','flBldgInvDmgFinal','OccupancyTypes']
         DDFTables = {}
         for DDF in DDFAssign:
-            # test = os.path.join(LUT_Dir,DDF+'.csv')
-            # print(test)
             with open(os.path.join(LUT_Dir,DDF+'.csv'), newline='') as csvfile:
                 file = csv.DictReader(csvfile)
                 DDFTable = [row for row in file]
@@ -32,7 +28,6 @@ def process(input,fmap):
         countie3 = [0,0,0,0]
         counter = 0
         counter2 = 0
-
         DDFDefault = ['flBldgStructDmgFn_DDF','flBldgContDmgFn_DDF','flBldgInvDmgFn_DDF']
         DDFDefaultTables = {}
         for DDF in DDFDefault:
@@ -48,7 +43,7 @@ def process(input,fmap):
         with open(input, newline='') as csvfile:
             field_names = field_names + [field for field in [SOID,BDDF_ID,CDDF_ID,IDDF_ID] if field not in field_names]
             field_names.append('DDF_Validity')
-            print(field_names)
+            #print(field_names)
             writer = csv.DictWriter(open(output, 'w'), delimiter=',', lineterminator='\n', fieldnames = field_names)
             file = csv.DictReader(csvfile)
             for row in file:
@@ -56,22 +51,24 @@ def process(input,fmap):
                     value = 0
                     basement = 1 if int(row[foundationtype]) == 4 else 0
                     if row.get(SOID) == None and SOID != '':
+                        
+                        #Loop to generate the SOCID
                         for line in DDFTables['SOoccupId_Occ_Xref']:
-                            compare = line['NumStoriesInt']
+                            compare = line['NumStoriesInt']                            
                             if len(compare) > 1 and int(line['Basement']) == basement and line['Occupancy'].strip() == row[OCC]:
                                 if '-' in compare:
                                     if int(compare[0]) <= int(row[NumStories]) <= int(compare[-1]):
                                         value = line['SOccupId'].strip()
                                         row[SOID] = value
                                         countie[0] = countie[0] + 1
-                                        row['DDF_Validity'] = None
+                                        row['DDF_Validity'] = None                                        
                                         break
                                 elif '>' in compare:
                                     if int(row[NumStories]) > int(compare[1]):
                                         value = line['SOccupId'].strip()
                                         row[SOID] = value
                                         countie[0] = countie[0] + 1
-                                        row['DDF_Validity'] = None
+                                        row['DDF_Validity'] = None                                      
                                         break
                                 elif '<' in compare:
                                     if int(row[NumStories]) < int(compare[1]):
@@ -79,21 +76,21 @@ def process(input,fmap):
                                         row[SOID] = value
                                         countie[0] = countie[0] + 1
                                         row['DDF_Validity'] = None
-                                        break
+                                        break                                       
                                 elif 'any' in compare:
 #                                    if int(row[NumStories]) < int(compare[1]):
                                     value = line['SOccupId'].strip()
                                     row[SOID] = value
                                     countie[0] = countie[0] + 1
                                     row['DDF_Validity'] = None
-                                    break                                        
+                                    break
                             elif len(compare) == 1 and line['Occupancy'].strip() == row[OCC] and int(compare) == int(row[NumStories]) and int(line['Basement']) == basement:
                                 value = line['SOccupId'].strip()
                                 row[SOID] = value
                                 countie[0] = countie[0] + 1
-                                row['DDF_Validity'] = None
+                                row['DDF_Validity'] = None                            
                                 break
-                                
+                    #BUILDING DDF                        
                     if row.get(BDDF_ID) == None and BDDF_ID != '':
                         for line in DDFTables['flBldgStructDmgFinal']:
                             if line['SOccupId'].strip() == value and int(line[HazardType]) == 1:
@@ -102,19 +99,21 @@ def process(input,fmap):
                                 row['DDF_Validity'] = None
                                 break
                             
-                    elif row.get(BDDF_ID) != None and BDDF_ID != '':
-                        countie3[1] += 1
-                        default = row[BDDF_ID].strip()
+                    elif row.get(BDDF_ID) != None and BDDF_ID != '':                        
+                        countie3[1] += 1                    
+                        default = row[BDDF_ID].strip()                       
                         row[BDDF_ID] = None
                         if default in DDFDefaultTables['flBldgStructDmgFn_DDF']:
                             row[BDDF_ID] = default
                             countie2[1] = countie2[1] + 1
                             row['DDF_Validity'] = None
-                            
-                        
+                    
+                    #CONTENT DDF                     
                     if row.get(CDDF_ID) == None and CDDF_ID != '':    
                         for line in DDFTables['flBldgContDmgFinal']:
                             if line['SOccupId'].strip() == value and int(line[HazardType]) == 1:
+                                #UKS 04/08/2020 - fixed BDDF_ID to CDDF_ID as this is for content DDF
+                                #Due to this the content DDF field was being wiped out and only building DDF field was being written
                                 row[CDDF_ID] = line['ContDmgFnId']
                                 countie[2] = countie[2] + 1
                                 row['DDF_Validity'] = None
@@ -125,13 +124,15 @@ def process(input,fmap):
                         default = row[CDDF_ID].strip()
                         row[CDDF_ID] = None
                         if default in DDFDefaultTables['flBldgContDmgFn_DDF']:
-                            row[BDDF_ID] = default
+                            row[CDDF_ID] = default
                             countie2[1] = countie2[1] + 1
                             row['DDF_Validity'] = None
-                            
+                    
+                    #INVENTORY DDF         
                     if row.get(IDDF_ID) == None and IDDF_ID != '':   
                         for line in DDFTables['flBldgInvDmgFinal']:
                             if line['SOccupId'].strip() == value and int(line[HazardType]) == 1:
+                                #UKS 04/08/2020 - fixed BDDF_ID to IDDF_ID as this is for inventory DDF
                                 row[IDDF_ID] = line['InvDmgFnId']
                                 countie[3] = countie[3] + 1
                                 row['DDF_Validity'] = None
@@ -142,7 +143,7 @@ def process(input,fmap):
                         default = row[IDDF_ID].strip()
                         row[IDDF_ID] = None
                         if default in DDFDefaultTables['flBldgInvDmgFn_DDF']:
-                            row[BDDF_ID] = default
+                            row[IDDF_ID] = default
                             countie2[1] = countie2[1] + 1
                             row['DDF_Validity'] = None
                         
