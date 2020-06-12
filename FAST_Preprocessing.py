2fb315175753122ea1888fb7a5df4f9844c87d6a
ujvalaks <ujvalak_in@yahoo.com>
2020-06-11 20:43:49 -0700

    nhrap-dev\hazpy reference
    Referencing code from the new nhrap-dev\hazpy repo

--- FAST_Preprocessing.py
+++ FAST_Preprocessing.py
@@ -3,11 +3,7 @@
 # call conda.bat activate hazus_env & start /min python .\src\FAST_PreProcess_run.py && exit
 from subprocess import call
 import os
-<<<<<<< HEAD
 call('conda.bat activate hazus_env & start /min python ./src/FAST_PreProcess_run.py', shell=True)
-=======
-call('CALL conda.bat activate hazus_env & start /min python ./src/FAST_PreProcess_run.py', shell=True)
->>>>>>> 0fa523095caa118a9662c3efc82cf4a817bcb19c
 if os.path.exists("FAST_Preprocessing.bat"):
     os.remove("FAST_Preprocessing.bat")
 else:
