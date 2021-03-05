f638158c45aff8f16f41f089344e09f4b12d75e8
NiyamITCLindeman <clindeman@niyamit.com>
2021-03-05 09:33:42 -1000

    Remove preprocessing
    Removed demo.avi from help, FAST_Preprocessing.py from root and renamed gui_process.py and pre_process.py by adding a notinuse suffix in Python_env.

--- FAST_Preprocessing.py
+++
@@ -1,7 +0,0 @@
-from src.manage import Manage
-
-if __name__=='__main__':
-    manage = Manage()
-    app_path = 'Python_env/gui_process.py'
-    update_path = 'src/update.py'
-    manage.startApp(app_path, update_path)
